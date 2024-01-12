a2560aae8e853e3ed48c97d74e43c1e96c0005cf
plan1objects <plan1object@gmail.com>
2024-01-12 16:18:01 +0900

    challgne-05_08

---
+++ ch_07_2-02_2024_01_11.js
@@ -0,0 +1,6 @@
+for(let i = 1; i < 5; i++){
+    console.log('*'.repeat(i));
+}
+for(let i = 5; i > 0; i--){
+    console.log('*'.repeat(i));
+}
---
+++ ch_07_2-03_2024_01_11.js
@@ -0,0 +1,6 @@
+for(let i = 1; i < 6; i++){
+    console.log(' '.repeat(6 - i) + '*'.repeat(2 * i - 1));
+}
+for(let i = 6; i > 0; i--){
+    console.log(' '.repeat(6 - i) + '*'.repeat(2 * i - 1));
+}
